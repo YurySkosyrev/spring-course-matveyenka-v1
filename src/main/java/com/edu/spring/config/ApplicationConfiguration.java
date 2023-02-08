0d517f280b409401773dbfc7b98f2d44e9dcb8f0
Yury Skosyrev <skosyrev-87@mail.ru>
2023-02-08 13:54:38 +0300

    Step 4.5/23

--- src/main/java/com/edu/spring/config/ApplicationConfiguration.java
+++ src/main/java/com/edu/spring/config/ApplicationConfiguration.java
@@ -1,13 +1,16 @@
 package com.edu.spring.config;
 
+import com.edu.spring.database.pool.ConnectionPool;
 import com.edu.spring.database.repository.CrudRepository;
-import org.springframework.context.annotation.ComponentScan;
+import com.edu.spring.database.repository.UserRepository;
+import com.edu.web.config.WebConfiguration;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.*;
 import org.springframework.context.annotation.ComponentScan.Filter;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
+@Import(WebConfiguration.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "com.edu.spring",
@@ -19,4 +22,25 @@ import org.springframework.stereotype.Component;
         }
 )
 public class ApplicationConfiguration {
+
+        @Bean
+        public ConnectionPool pool2(@Value("${db.username}") String username) {
+                return new ConnectionPool(username, 28);
+        }
+
+        @Bean
+        public ConnectionPool pool3() {
+                return new ConnectionPool("test-pool", 25);
+        }
+
+        @Bean
+        @Profile("!prod")
+        public UserRepository userRepository2(ConnectionPool pool2){
+                return new UserRepository(pool2);
+        }
+
+        @Bean
+        public UserRepository userRepository3(){
+                return new UserRepository(pool3());
+        }
 }
