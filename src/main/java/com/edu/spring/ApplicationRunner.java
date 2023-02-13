4bca2954402596ffd44e4add97beff679e7b839e
Yury Skosyrev <skosyrev-87@mail.ru>
2023-02-13 09:14:24 +0300

    Step 6.3/23

--- src/main/java/com/edu/spring/ApplicationRunner.java
+++ src/main/java/com/edu/spring/ApplicationRunner.java
@@ -6,6 +6,8 @@ import com.edu.spring.database.repository.CompanyRepository;
 import com.edu.spring.database.repository.CrudRepository;
 import com.edu.spring.service.CompanyService;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -13,25 +15,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
 
+@SpringBootApplication
 public class ApplicationRunner {
     public static void main(String[] args) {
-
-        String value = "hello";
-        System.out.println(CharSequence.class.isAssignableFrom(value.getClass()));
-        System.out.println(BeanFactoryPostProcessor.class.isAssignableFrom(value.getClass()));
-        System.out.println(Serializable.class.isAssignableFrom(value.getClass()));
-
-        try (var context = new AnnotationConfigApplicationContext()) {
-
-            context.register(ApplicationConfiguration.class);
-            context.getEnvironment().setActiveProfiles("prod");
-            context.refresh();
-
-            var connectionPool = context.getBean("pool1", ConnectionPool.class);
-            System.out.println(connectionPool);
-
-            var companyService = context.getBean(CompanyService.class);
-            System.out.println(companyService.findById(1));
-        }
+        SpringApplication.run(ApplicationRunner.class, args);
     }
 }
