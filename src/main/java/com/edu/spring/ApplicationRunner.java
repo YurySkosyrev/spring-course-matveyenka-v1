0ce0f4df1451bd76533b41c13a5cbeaec57dac31
YurySkosyrev <skosyrev-87@mail.ru>
2023-02-19 15:50:33 +0300

    Step 6.8/23

--- src/main/java/com/edu/spring/ApplicationRunner.java
+++ src/main/java/com/edu/spring/ApplicationRunner.java
@@ -1,9 +1,13 @@
 package com.edu.spring;
 
+import com.edu.spring.config.DataBaseProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class ApplicationRunner {
     public static void main(String[] args) {
 
@@ -26,5 +30,7 @@ public class ApplicationRunner {
 //        }
         var context = SpringApplication.run(ApplicationRunner.class, args);
         System.out.println(context.getBeanDefinitionCount());
+        System.out.println(context.getBean("pool1"));
+        System.out.println(context.getBean(DataBaseProperties.class));
     }
 }
--- src/main/java/com/edu/spring/config/DataBaseProperties.java
+++ src/main/java/com/edu/spring/config/DataBaseProperties.java
@@ -1,18 +1,29 @@
 package com.edu.spring.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
 import java.util.List;
+import java.util.Map;
 
-public class DataBaseProperties {
-    private String username;
-    private String password;
-    private String driver;
-    private String url;
-    private String hosts;
-    private PoolProperties pool;
-    private List<PoolProperties> pools;
 
-    public static class PoolProperties {
-        private Integer size;
-        private Integer timeout;
+//@Data
+//@NoArgsConstructor
+//@Component
+@ConstructorBinding
+@ConfigurationProperties(prefix = "db")
+public record DataBaseProperties(String username,
+                                 String password,
+                                 String driver,
+                                 String url,
+                                 String hosts,
+                                 PoolProperties pool,
+                                 List<PoolProperties> pools,
+                                 Map<String, Object> properties) {
+
+    //    @Data
+    //    @NoArgsConstructor
+    public static record PoolProperties(Integer size,
+                                 Integer timeout) {
     }
 }
