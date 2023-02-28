747cab0bb8458a5a5cf67786111619fb6147b5b2
Yury Skosyrev <skosyrev-87@mail.ru>
2023-02-28 09:13:24 +0300

    Step 8.4/23

--- src/test/java/com/edu/spring/integration/service/CompanyServiceIT.java
+++ src/test/java/com/edu/spring/integration/service/CompanyServiceIT.java
@@ -1,17 +1,22 @@
 package com.edu.spring.integration.service;
 
 import com.edu.spring.ApplicationRunner;
+import com.edu.spring.config.DataBaseProperties;
 import com.edu.spring.database.entity.Company;
 import com.edu.spring.dto.CompanyReadDTO;
+import com.edu.spring.integration.annotation.IT;
 import com.edu.spring.listner.entity.EntityEvent;
 import com.edu.spring.service.CompanyService;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestConstructor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
@@ -24,13 +29,16 @@ import static org.mockito.Mockito.*;
 //@ContextConfiguration(classes = ApplicationRunner.class,
 //initializers = ConfigDataApplicationContextInitializer.class)
 
-@SpringBootTest
+@IT
+@RequiredArgsConstructor
 public class CompanyServiceIT {
 
     private static final Integer COMPANY_ID = 1;
 
-    @Autowired
-    private CompanyService companyService;
+
+    private final CompanyService companyService;
+
+    private final DataBaseProperties dataBaseProperties;
 
     @Test
     void findById() {
