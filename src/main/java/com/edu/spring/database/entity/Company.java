c40e84f82665470cf06bef0cfa7e5268e2ded5f3
YurySkosyrev <skosyrev-87@mail.ru>
2023-03-02 22:38:07 +0300

    Step 9.2/23 part 2

--- src/main/java/com/edu/spring/database/entity/Company.java
+++ src/main/java/com/edu/spring/database/entity/Company.java
@@ -1,10 +1,21 @@
 package com.edu.spring.database.entity;
 
-import javax.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "company")
-public class Company {
+public class Company implements BaseEntity<Integer> {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,4 +23,11 @@ public class Company {
 
     @Column(unique = true, nullable = false)
     private String name;
+
+    @Builder.Default
+    @ElementCollection
+    @CollectionTable(name = "company_locales", joinColumns = @JoinColumn(name = "company_id"))
+    @MapKeyColumn(name = "lang")
+    @Column(name = "description")
+    private Map<String, String> locales = new HashMap<>();
 }
---
+++ src/main/java/com/edu/spring/database/entity/User.java
@@ -0,0 +1,31 @@
+package com.edu.spring.database.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "users")
+public class User implements BaseEntity<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private LocalDate birthDate;
+
+    private String firstname;
+
+
+}
